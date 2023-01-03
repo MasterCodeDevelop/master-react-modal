47f97f0fe169ec49e801c05dc0ff2d1aa9c2e17c
master <master.code.develop@gmail.com>
2023-01-03 22:17:42 +0100

    modal open and close

--- src/examples/App.jsx
+++ src/examples/App.jsx
@@ -4,12 +4,13 @@ import './App.scss';
 
 export default function App() {
   const [openModal, setOpenModal] = useState(false);
+  const closeModal = () => setOpenModal(false);
   return (
     <div className="app">
       <button className="btn btn-primary" onClick={() => setOpenModal(true)}>
         Show Modal
       </button>
-      <Modal isOpen={openModal} />
+      <Modal isOpen={openModal} close={closeModal} />
     </div>
   );
 }
--- src/lib/components/Modal.jsx
+++ src/lib/components/Modal.jsx
@@ -3,17 +3,24 @@ import './Modal.scss';
 import PropTypes from 'prop-types';
 
 /**
- *
+ * Modal, this is modal container.
  * @param {JSON} props
- * isOpen: boolean
- *
- * @returns JSX
+ * (isOpen: boolean) ==> modal state to show or not
+ * (close: function) ==> close Modal
+ * @returns JSX Element
  */
-function Modal({ isOpen }) {
-  return <div>Modal {isOpen ? 'isOpen' : 'isClose'} </div>;
+function Modal({ isOpen, close }) {
+  const classModal = `modal ${isOpen ? 'isOpen' : ''}`;
+  return (
+    <div className={classModal}>
+      <div className="modal-content">Modal Content</div>
+      <div onClick={() => close()} className="modal-close"></div>
+    </div>
+  );
 }
 Modal.propTypes = {
   isOpen: PropTypes.bool,
+  close: PropTypes.func,
 };
 
 export { Modal };
